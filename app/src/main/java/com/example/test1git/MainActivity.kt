d4bc1e460da2c1910aee4f42d47eb356596cfec5
Yaseen Al-Shaibani <yasenshibane77@gmail.com>
2020-12-29 19:37:45 +0300

    init commit

--- app/src/main/java/com/example/test1git/MainActivity.kt
+++ app/src/main/java/com/example/test1git/MainActivity.kt
@@ -7,15 +7,23 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     lateinit var textView:TextView
+    lateinit var textViewY:TextView
     lateinit var btn:Button
+    lateinit var btnY:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView=findViewById(R.id.tv)
+        textViewY=findViewById(R.id.tv_yaseen)
         btn= findViewById(R.id.GitBtn)
 
         btn.setOnClickListener {
             textView.text= "Welcome GitHub"
+        }
+        btnY= findViewById(R.id.GitBtn_yaseen)
+
+        btnY.setOnClickListener {
+            textViewY.text= "Welcome GitHub yaseen"
         }
     }
 }
